dc3e3c43d843f2290b2783593c30c32927d137ea
Joshua Stowers <5009837+stowersjoshua@users.noreply.github.com>
2020-03-21 14:51:34 -0500

    Create empty initializer from install generator
    We can build this out more later, but having it in will help to show the purpose of this generator - to quickly get everything devs need in place automatically.
    
    Signed-off-by: Joshua Stowers <5009837+stowersjoshua@users.noreply.github.com>

--- lib/generators/accountability/install_generator.rb
+++ lib/generators/accountability/install_generator.rb
@@ -3,8 +3,16 @@ require 'rails/generators/active_record'
 module Accountability
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      # Executes with: `rails generate accountability:install`
+      # Note: All public instance methods will be called sequentially
+
       include ActiveRecord::Generators::Migration
       source_root File.join(__dir__, 'templates')
+
+      def create_initializer_file
+        initializer_content = "Accountability.configure { |_config| }"
+        create_file "config/initializers/accountability.rb", initializer_content
+      end
 
       def copy_migration
         migration_template 'migration.rb', 'db/migrate/create_accountability_tables.rb', migration_version: migration_version
